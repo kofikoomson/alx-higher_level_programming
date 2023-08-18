7e28c2ef4022b944f78cf77ef551099bce7d9928
kofikoomson <kofikoomson94@gmail.com>
2023-08-17 20:37:53 -0700

    replace occurrence

--- 0x04-python-more_data_structures/0-square_matrix_simple.py
+++ 0x04-python-more_data_structures/0-square_matrix_simple.py
@@ -2,10 +2,7 @@
 
 
 def square_matrix_simple(matrix=[]):
-    """
-    wordA function that computes the square
-    value of all integers of a matrix.
-    """
+    
     new_matrix = []
     for col in matrix:
         result = list(map(lambda x: x**2, col))
