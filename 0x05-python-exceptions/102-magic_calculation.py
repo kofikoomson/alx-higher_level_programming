eaea20075eab5c26b6d18c472f050068df1e89fd
kofikoomson <kofikoomson94@gmail.com>
2023-08-30 10:47:10 -0700

    extended files

---
+++ 0x05-python-exceptions/102-magic_calculation.py
@@ -0,0 +1,14 @@
+#!/usr/bin/python3
+
+def magic_calculation(a, b):
+    product = 0
+    for i in range(1, 3):
+        try:
+            if i > a:
+                raise Exception('Too far')
+            else:
+                product += a ** b / i
+        except:
+            product = b + a
+            break
+    return (product)
